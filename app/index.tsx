1cac04032736085eed7525c5c5b6cfdd0bb3eab5
imbananacrazy <bananacrazyness21@gmail.com>
2026-02-27 10:04:20 -0700

    use state

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import ToDoForm from "@/components/to-do-form";
 import ToDoList from "@/components/to-do-list";
-import React from "react";
+import React, { use } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
-  const tasks:string[] = ["Do laundry", "Go to gym", "Walk dog"];
+  const [tasks, setTasks] = useState<string[]>([
+    "Do laundry",
+    "Go to gym",
+    "Walk dog",
+  ]);
 
   return (
     <SafeAreaView>
