abf5af20895e3464b87455043d508d0a5fcfeaba
imbananacrazy <bananacrazyness21@gmail.com>
2026-02-27 10:00:30 -0700

    removed comment and tasks converted to <li>

--- components/to-do-list.tsx
+++ components/to-do-list.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Pressable, ScrollView, StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet, Text } from "react-native";
 
 const ToDoList = ({ tasks }: { tasks: string[] }) => {
   return (
     <ScrollView>
-      {/* First task completed, second task not, third task completed. i know i have it hard-coded and thats not optimal, but it works for this lab */}
       {tasks.map((task, index) => (
-        <Pressable key={index} style={styles.task}>
+        <li key={index} style={styles.task}>
           <Text style={styles.taskText}>{task}</Text>
-        </Pressable>
+        </li>
       ))}
     </ScrollView>
   );
